2691bc809d8235ab2a13ee364b9908972b0e37ce
jdurn3 <jdurn3@gmail.com>
2024-03-11 12:43:40 -0600

    installed more needed packages

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
-const { peerProxy } = require('./peerProxy.js');
+//const { peerProxy } = require('./peerProxy.js');
 
 const authCookieName = 'token';
 
@@ -114,4 +114,4 @@ const httpService = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-peerProxy(httpService);
+//peerProxy(httpService);
