5a3cba4fe20beb04cd817f151836b1342131d850
jdurn3 <jdurn3@gmail.com>
2024-03-19 14:24:32 -0600

    added database

--- public/login.js
+++ public/login.js
@@ -10,46 +10,46 @@ document.getElementById('loginForm').addEventListener('submit', function (event)
     document.getElementById('displayedUser').innerText = username;
 });
 
-// async function loginUser() {
-//     loginOrCreate(`/api/auth/login`);
-// }
+async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+}
 
-// async function createUser() {
-//     loginOrCreate(`/api/auth/create`);
-// }
+async function createUser() {
+    loginOrCreate(`/api/auth/create`);
+}
 
-// async function loginOrCreate(endpoint) {
-//     const userName = document.querySelector('#userName')?.value;
-//     const password = document.querySelector('#userPassword')?.value;
-//     const response = await fetch(endpoint, {
-//         method: 'post',
-//         body: JSON.stringify({ email: userName, password: password }),
-//         headers: {
-//         'Content-type': 'application/json; charset=UTF-8',
-//         },
-// })
-// };
+async function loginOrCreate(endpoint) {
+    const userName = document.querySelector('#userName')?.value;
+    const password = document.querySelector('#userPassword')?.value;
+    const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({ email: userName, password: password }),
+        headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        },
+})
+};
 
-// function logout() {
-//     localStorage.removeItem('userName');
-//     fetch(`/api/auth/logout`, {
-//         method: 'delete',
-//     }).then(() => (window.location.href = '/'));
-//     }
+function logout() {
+    localStorage.removeItem('userName');
+    fetch(`/api/auth/logout`, {
+        method: 'delete',
+    }).then(() => (window.location.href = '/'));
+    }
 
-// async function getUser(email) {
-//     // See if we have a user with the given email.
-//     const response = await fetch(`/api/user/${email}`);
-//     if (response.status === 200) {
-//         return response.json();
-//     }
+async function getUser(email) {
+    // See if we have a user with the given email.
+    const response = await fetch(`/api/user/${email}`);
+    if (response.status === 200) {
+        return response.json();
+    }
 
-//     return null;
-//     }
+    return null;
+    }
 
-// function setDisplay(controlId, display) {
-//     const playControlEl = document.querySelector(`#${controlId}`);
-//     if (playControlEl) {
-//         playControlEl.style.display = display;
-//     }
-//     }
+function setDisplay(controlId, display) {
+    const playControlEl = document.querySelector(`#${controlId}`);
+    if (playControlEl) {
+        playControlEl.style.display = display;
+    }
+    }
